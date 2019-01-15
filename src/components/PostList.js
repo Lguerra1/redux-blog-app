b44efbd70c39030255ec79053f82878506bd2cc7
unknown <Nightmachine@gmail.com>
2019-01-15 00:41:20 -0700

    added render method for posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,11 +7,26 @@ class PostList extends Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
-    render() {
-        console.log(this.props.post)
-        return (
 
-            <div>Post List</div>
+    renderList() {
+        return this.props.post.map(post => {
+            return (
+                <div className='item' key={post.id}>
+                    <i className='large middle aligned icon user' />
+                    <div className='content'>
+                        <div className='description'>
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className='ui relaxed divided list'>{this.renderList()}</div>
         )
     }
 }
