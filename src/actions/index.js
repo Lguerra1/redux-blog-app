5174852b265af5f9012d406ddfe53011bd1ee7cf
unknown <Nightmachine@gmail.com>
2019-01-22 22:30:40 -0700

    added ownProps and userHeader

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,12 @@ export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({
-        type: "FETCH_POSTS",
-        payload: response.data
+        type: "FETCH_POSTS", payload: response.data
     })
 };
-  
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+};
